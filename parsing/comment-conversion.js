d8fe2fb99c10737fbaffe44e9257a724c2ef1e86
Luke Savage <luke.savage92@gmail.com>
2015-08-29 18:53:51 -0500

    Added type hash map and helper function to convert assertion symbols to usable syntax

--- parsing/comment-conversion.js
+++ parsing/comment-conversion.js
@@ -1,5 +1,15 @@
 var extractValues = require('extract-values');
 
+var assertionTypeMap = {
+  '==': 'equal',
+  '===': 'deepEqual'
+};
+
+//Helper takes string as an input, matches to hash map and returns the converted value
+var convertAssertionType = function(type) {
+  return assertionTypeMap[type];
+};
+
 var extractTestDetails = function(parsedComment) {
   var extractedDetails = {};
 
@@ -13,6 +23,9 @@ var extractTestDetails = function(parsedComment) {
   //Loop over all assertions and use pattern matching to extract the atomic units
   parsedComment.assertions.forEach(function(assertion) {
     assertionParts = extractValues(assertion, '{assertionInput} {assertionType} {assertionOutput} ({assertionMessage})');
+
+    //Convert assertion type from symbol to usable syntax
+    assertionParts.assertionType = convertAssertionType(assertionParts.assertionType);
     extractedDetails.assertions.push(assertionParts);
   });
 
