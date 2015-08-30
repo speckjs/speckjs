dd0040ad5e009b2406545bb9a476337a39bad0a9
Nick Balestra <nick@balestra.ch>
2015-08-29 18:24:20 -0700

    Added a demo fixture

---
+++ src/demo.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+  // test > sum function
+  // # sum(1,3) == 4 (returnt the sum of both params)
+  // # sum(10,10) == 20 (return the sum of both params)
+  sum: function(a, b) {
+    return a + b;
+  },
+
+  // test > product function
+  // # product(2,3) == 6 (returnt the product of both params)
+  // # product(10,10) == 100 (return the product of both params)
+  // # product(2,25) == 50 (return the product of both params)
+  product: function(a, b) {
+    return a * b;
+  }
+  // test foo function
+  // test foo > function
+
+  // # sum(1,3) == 4 (returnt the sum of both params)
+
+  // test > foo function
+  // test > bar function
+};
