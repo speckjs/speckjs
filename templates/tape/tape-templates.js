9a5ada3ae66b68c8bc8b387d2d6da2fdcca3f390
Greg RV <gregvarias@gmail.com>
2015-08-31 01:29:14 -0700

    Add file containing all initial tape assertion templates currently supported (pass, fail, equals). Add utility function to create actual javascript using data that comes from parsed comments.

---
+++ templates/tape/tape-templates.js
@@ -0,0 +1,20 @@
+// UTILITY TEMPLATES
+var tempRequire = "var {{=it.varName}} = require('{{=it.module}}');";
+
+// BASE TEMPLATE FOR TESTS (deliberately incomplete statement - closed in util fx)
+var baseTemplate =
+"test('{{=it.testTitle}}', function(t) { \
+t.plan({{=it.assertions}}); \ ";
+
+// INDIVIDUAL ASSERTION TEMPLATES
+var passTemplate = "t.{{=it.assertionType}}('{{=it.assertionMessage}}'); \n";
+var failTemplate = "t.{{=it.assertionType}}('{{=it.assertionMessage}}'); \n";
+var equalsTemplate = "t.{{=it.assertionType}}({{=it.assertionOutput}}, file.{{=it.assertionInput}}, \'{{=it.assertionMessage}}\'); \n";
+
+module.exports = {
+  require: tempRequire,
+  base: baseTemplate,
+  pass: passTemplate,
+  fail: failTemplate,
+  equals: equalsTemplate
+};
