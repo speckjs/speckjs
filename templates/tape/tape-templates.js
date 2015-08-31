fbd2383b297677d162d57064bedbc6162bd1b5c6
Greg RV <gregvarias@gmail.com>
2015-08-31 11:09:32 -0700

    Complete templates for tape pass, fail assertions.

--- templates/tape/tape-templates.js
+++ templates/tape/tape-templates.js
@@ -1,7 +1,7 @@
 // UTILITY TEMPLATES
 var tempRequire = "var {{=it.varName}} = require('{{=it.module}}');";
 
-// BASE TEMPLATE FOR TESTS (deliberately incomplete statement - closed in util fx)
+// BASE TEMPLATE FOR TESTS (deliberately missing ending syntax - closed in util fx)
 var baseTemplate =
 "test('{{=it.testTitle}}', function(t) { \
 t.plan({{=it.assertions}}); \ ";
@@ -9,8 +9,9 @@ t.plan({{=it.assertions}}); \ ";
 // INDIVIDUAL ASSERTION TEMPLATES
 var passTemplate = "t.{{=it.assertionType}}('{{=it.assertionMessage}}'); \n";
 var failTemplate = "t.{{=it.assertionType}}('{{=it.assertionMessage}}'); \n";
-var equalsTemplate = "t.{{=it.assertionType}}({{=it.assertionOutput}}, file.{{=it.assertionInput}}, \'{{=it.assertionMessage}}\'); \n";
+var equalsTemplate = "t.{{=it.assertionType}}({{=it.assertionOutput}}, file.{{=it.assertionInput}}, '{{=it.assertionMessage}}'); \n";
 
+// EXPORT TEMPLATES
 module.exports = {
   require: tempRequire,
   base: baseTemplate,
