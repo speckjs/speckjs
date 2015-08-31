fb894a702959a8c6ba2fdc0c0a912ceff8dc36de
Luke Savage <luke.savage92@gmail.com>
2015-08-31 17:39:25 -0500

    Refactored extractTestDetails using map and updated type hash map

--- parsing/comment-conversion.js
+++ parsing/comment-conversion.js
@@ -2,7 +2,9 @@ var extractValues = require('extract-values');
 
 var assertionTypeMap = {
   '==': 'equal',
-  '===': 'deepEqual'
+  '===': 'deepEqual',
+  '!==': 'notEqual',
+  '!===': 'notDeepEqual'
 };
 
 //Helper takes string as an input, matches to hash map and returns the converted value
@@ -11,20 +13,19 @@ var convertAssertionType = function(type) {
 };
 
 var extractTestDetails = function(parsedAssertions) {
-  var extractedAssertions = [];
   var assertionParts;
 
   //Loop over all assertions and use pattern matching to extract the atomic units
-  parsedAssertions.forEach(function(assertion) {
+  return parsedAssertions.map(function(assertion) {
     assertionParts = extractValues(assertion, '{assertionInput} {assertionType} {assertionOutput} ({assertionMessage})');
 
     //Convert assertion type from symbol to usable syntax
     assertionParts.assertionType = convertAssertionType(assertionParts.assertionType);
-    extractedAssertions.push(assertionParts);
+    return assertionParts;
   });
-
-  return extractedAssertions;
 };
+
+// console.log(extractTestDetails(test));
 
 module.exports = {
   extractTestDetails: extractTestDetails
