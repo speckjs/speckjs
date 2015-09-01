34d5334d6abbba7b869f5825ffa96152bae535d3
Zach Sebag <zsebag@gmail.com>
2015-09-01 12:22:07 -0500

    Added utility function test for object with 0 assertions

--- tests/tapeTemplateTests.js
+++ tests/tapeTemplateTests.js
@@ -1,41 +1,27 @@
 var test = require('tape');
+var template = require('template-utils.js');
+var tapeTemplates = require('tape-templates.js');
 
 // TEST FIXTURE
-var dataObj = {
-  specType : 'tape',
-  specFileSrc : 'app.js',
-  tests : [
-    { testTitle: 'sum function',
-      assertions: [
-        { assertionMessage: 'return the sum of both params',
-          assertionType: 'equals',
-          assertionInput: 'sum(6, 7)',
-          assertionOutput: '13'
-        },
-        { assertionMessage: 'return the sum of both params',
-          assertionType: 'equals',
-          assertionInput: 'sum(8, 9)',
-          assertionOutput: '17'
-        }
-      ]
-    },
-    { testTitle: 'multiply function',
-      assertions: [
-        { assertionMessage: 'return the product of both params',
-          assertionType: 'equals',
-          assertionInput: 'mult(4, 5)',
-          assertionOutput: '20'
-        }
-      ]
-    }
-  ]
-};
 
 test('tape template function', function (t) {
-    t.plan(2); // How many tests?
-        //It takes an empty object and outputs an empty test block
-        //It takes a properly formatted object and outputs a formatted test block
-        //It takes one test
-        //It takes multiple tests
-        //If a field is missing, return error, missing field
+    t.plan(3); // How many tests
+    //Dummy data for parsed comment
+    var emptyTestObj = {
+      specType : 'tape',
+      specFileSrc : 'app.js',
+      tests : [
+        { testTitle: 'sum function',
+          assertions: []
+        }
+       ]
+    };
+
+    var emptyTestBlock = "test('sum function', function (t) {" +
+                          "})";
+    //It takes an empty object and outputs an empty test block
+    t.equal(template.addTestDataToBaseTemplate(tapeTemplates.baseTemplate, emptyTestObj), emptyTestBlock, 'Takes an test with 0 assertion and outputs a base template');
+    //It takes one test block and produces a properly formatted tape test
+    //It takes multiple test blocks and produces a properly formatted tape test
+    //If a field is missing, return error, missing field
 });
