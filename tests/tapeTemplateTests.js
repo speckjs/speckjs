683b29c0cbb335aab0e092bf01981ede742cbadc
Zach Sebag <zsebag@gmail.com>
2015-09-01 12:25:59 -0500

    Added template utility function tape test for object with multiple tests

--- tests/tapeTemplateTests.js
+++ tests/tapeTemplateTests.js
@@ -7,6 +7,35 @@ var tapeTemplates = require('tape-templates.js');
 test('tape template function', function (t) {
     t.plan(2); // How many tests?
     //Dummy data for parsed comment
+    var dataObj = {
+    specType : 'tape',
+    specFileSrc : 'app.js',
+    tests : [
+      { testTitle: 'sum function',
+        assertions: [
+          { assertionMessage: 'return the sum of both params',
+            assertionType: 'equals',
+            assertionInput: 'sum(6, 7)',
+            assertionOutput: '13'
+          },
+          { assertionMessage: 'return the sum of both params',
+            assertionType: 'equals',
+            assertionInput: 'sum(8, 9)',
+            assertionOutput: '17'
+          }
+        ]
+      },
+      { testTitle: 'multiply function',
+        assertions: [
+          { assertionMessage: 'return the product of both params',
+            assertionType: 'equals',
+            assertionInput: 'mult(4, 5)',
+            assertionOutput: '20'
+          }
+        ]
+      }
+    ]
+    };
     var emptyTestObj = {
       specType : 'tape',
       specFileSrc : 'app.js',
@@ -37,6 +66,13 @@ test('tape template function', function (t) {
       }
      ]
     };
+    var normalTestBlock = "test('sum function', function (t) {" +
+                            "t.equal(sum(6, 7), 13, 'return the sum of both params')" +
+                            "t.equal(sum(8, 9), 17, 'return the sum of both params')"+
+                          ")}" +
+                          "test('multiply function', function (t) {" +
+                            "t.equal(multiply(4, 5), 20, 'return the product of both params')" +
+                          ")}";
     var singleTestBlock = "test('sum function', function (t) {" +
                             "t.equal(sum(6, 7), 13, 'return the sum of both params')" +
                             "t.equal(sum(8, 9), 17, 'return the sum of both params')"+
@@ -48,5 +84,6 @@ test('tape template function', function (t) {
     //It takes one test block and produces a properly formatted tape test
     t.equal(template.addTestDataToBaseTemplate(tapeTemplates.baseTemplate, singleTestObj), emptyTestBlock, 'Takes an test with 0 assertion and outputs a base template');
     //It takes multiple test blocks and produces a properly formatted tape test
+    t.equal(template.addTestDataToBaseTemplate(tapeTemplates.baseTemplate, dataObj), normalTestBlock, 'Takes a properly formatted object and outputs a formatted test block');
     //If a field is missing, return error, missing field
 });
