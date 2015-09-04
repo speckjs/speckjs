5869f8b118f9b0c9b9a3899929965acd1c46af05
Luke Savage <luke.savage92@gmail.com>
2015-09-04 14:28:00 -0500

    Extended Jasmine functionality with deepEqual template

--- templates/jasmine/jasmine-templates.js
+++ templates/jasmine/jasmine-templates.js
@@ -1,5 +1,6 @@
 // Utility templates
 var tempRequire = 'var {{=it.varName}} = require(\'{{=it.module}}\');';
+var assertRequire = 'var assert = require(\'assert\');';
 
 // Base template
 var baseTemplate = 'describe(\'{{=it.testTitle}}\', function() { ';
@@ -7,13 +8,15 @@ var baseTemplate = 'describe(\'{{=it.testTitle}}\', function() { ';
 // Individual assertion templates
 var equalTemplate = 'it(\'{{=it.assertionMessage}}\', function() {\nexpect(file.{{=it.assertionInput}}.toBe({{=it.assertionOutput}}))\n});';
 var notEqualTemplate = 'it(\'{{=it.assertionMessage}}\', function() {\nexpect(file.{{=it.assertionInput}}.not.toBe({{=it.assertionOutput}}))\n});';
+var deepEqualTemplate = 'it(\'{{=it.assertionMessage}}\', function() {\nvar pass;\ntry {\npass = true;\nassert.deepEqual(file.{{=it.assertionInput}}, {{=it.assertionOutput}});\n} catch (e) {\npass = false;\n}\nexpect(pass).toBe(true);\n});\n';
 
 module.exports = {
   require: tempRequire,
+  assert: assertRequire,
   base: baseTemplate,
   equal: equalTemplate,
-  notEqual: notEqualTemplate
+  notEqual: notEqualTemplate,
+  deepEqual: deepEqualTemplate
   //Commented these out since we will be implementing them soon
-  //deepEqual: deepEqualTemplate,
   //notDeepEqual: notDeepEqualTemplate
 };
