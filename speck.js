6c5139b6b355d407e46d1ec91441bafc42796122
Luke Savage <luke.savage92@gmail.com>
2015-09-08 17:25:54 -0500

    Added docco generated html page and reformatted comments in speck.js

--- speck.js
+++ speck.js
@@ -1,11 +1,6 @@
-/*
-  ============================================================
-  Thatsweet.js: transforming inline comments into unit tests
-  ============================================================
-  Thatsweet produce sjs syntax file, that can be compiled into
-  testSpec.js files via sweet.js ans a set of ad-hoc macros.
-*/
+//`SpeckJS V0.0.3`
 
+// Dependecies and default options for build function
 var comments = require('./parsing/parse-comments.js');
 var extract = require('./parsing/comment-conversion.js');
 var tapeTemps = require('./templates/tape/tape-templates.js');
@@ -17,25 +12,17 @@ var defaultOptions = {
   onBuild: null
 };
 
-/*
-  Public: Gets a file object with SpeckJS-formatted comments.
-  Either returns a spec file string, or invokes a callback with the string.
-
-  file  - An object that contains information about the file to parse, including:
-    name - A String of the file name.
-    content - A String of a JavaScript file.
-  options  - An Object of additional parameters (optional).
-    testFW - A String of the desired test framework for output ('tape', 'jasmine')
-    onBuild - A Function that is called with the returned string representing a spec file.
-
-  Returns: If no onBuild function is provided, returns a string representing a spec file.
-*/
+//  Takes a file object with SpeckJS-formatted comments.
+//  Either returns a spec file string if no callback function provided, or invokes a callback with the string.
 var build = function build(file, options) {
   options = options || defaultOptions;
   var output;
+  //  Extracts the tests from the input file based on SpeckJS syntax.
   var tests = comments.parse(file.content).tests;
   var testsReadyToAssemble = tests.map(function(test) {
     var testDetails;
+    //  If assertions exist, gathers the atomic units of test assertions.
+    //  Otherwise, set to empty string to create blank test.
     if (test.assertions.length) {
       testDetails = extract.extractTestDetails(test.assertions);
     } else {
@@ -43,6 +30,7 @@ var build = function build(file, options) {
     }
     var utilData = tempUtils.prepDataForTemplating(options.testFW, file.name, test, testDetails);
     var jsTestString;
+    //  Assemble the assertions into a test depending on the testing framework specified.
     if (options.testFW === 'jasmine') {
       jsTestString = tempUtils.addTestDataToBaseTemplateJasmine(utilData, jasmineTemps.base);
     }
@@ -52,6 +40,7 @@ var build = function build(file, options) {
     return jsTestString;
   });
 
+  // Final assembly of tests, then if there is a callback it is used. Otherwise, just return the string output.
   output = tempUtils.assembleTestFile(file.name, testsReadyToAssemble, options.testFW);
 
   if (typeof options.onBuild === 'function') {
