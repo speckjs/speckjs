acf4219b4699f81d49052a8765de2faa56bd76b1
Zach Sebag <zsebag@gmail.com>
2015-09-08 17:33:37 -0500

    Added skeleton tests for utility file

---
+++ tests/utilityTests.js
@@ -0,0 +1,26 @@
+var test = require('tape');
+var eol = require('os').EOL;
+
+test('utility require function', function (t) {
+
+});
+
+test('Add test data to base tape tempalte function', function (t) {
+
+});
+
+test('Add test data to base jasmine template function', function (t) {
+
+});
+
+test('Prepare data for templating function', function (t) {
+
+});
+
+test('Prepare data for templating function', function (t) {
+
+});
+
+test('Assemble test file function', function (t) {
+
+});
