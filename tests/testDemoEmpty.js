9088b311a07f7de27e0f8b7c61724fc0829fad62
Zach Sebag <zsebag@gmail.com>
2015-09-08 14:17:31 -0500

    Added test files to use within internale tests

---
+++ tests/testDemoEmpty.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  // test > sum function
+  sum: function(a, b) {
+    return a + b;
+  }
+
+};
---
+++ tests/testDemoSingle.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+  // test > sum function
+  // # sum(6, 7) == 13 (return the sum of both params)
+  // # sum(8, 9) == 17 (return the sum of both params)
+  sum: function(a, b) {
+    return a + b;
+  }
+
+};
