df6125cfc8104e587c82f69e80217ac823b642c6
Zach Sebag <zsebag@gmail.com>
2015-09-08 18:03:28 -0500

    Added unit test for utility add requires function

--- tests/utilityTests.js
+++ tests/utilityTests.js
@@ -1,8 +1,11 @@
 var test = require('tape');
 var eol = require('os').EOL;
+var tempUtils = require('../templates/template-utils.js');
 
 test('utility require function', function (t) {
-
+  t.plan(2);
+  t.equal(tempUtils.addRequire('test', 'tape'), 'var test = require(\'tape\');' + eol, 'Adds a require statement for tape with the test var name');
+  t.equal(tempUtils.addRequire('assert', 'assert'), 'var assert = require(\'assert\');' + eol, 'Adds a require statement for node assert library');
 });
 
 test('Add test data to base tape tempalte function', function (t) {
@@ -10,10 +13,6 @@ test('Add test data to base tape tempalte function', function (t) {
 });
 
 test('Add test data to base jasmine template function', function (t) {
-
-});
-
-test('Prepare data for templating function', function (t) {
 
 });
 
