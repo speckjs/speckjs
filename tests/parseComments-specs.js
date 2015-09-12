393cdec1bf605b61ce5c6dd2013196f86cc7f41a
Zach Sebag <zsebag@gmail.com>
2015-09-11 19:04:47 -0500

    Added spec to test for an AST output from parse function

--- tests/parseComments-specs.js
+++ tests/parseComments-specs.js
@@ -11,6 +11,7 @@ test('parse function test', function(t) {
   var testString = fs.readFileSync(path.join(__dirname, 'testDemoSingle.js'), {encoding: 'utf8'});
   var testStringEmpty = fs.readFileSync(path.join(__dirname, 'testDemoEmpty.js'), {encoding: 'utf8'});
   t.equal(typeof comments.parse, 'function', 'Should be a function');
+  t.ok(comments.parse(testString), 'Should output an AST');
   t.deepEqual(comments.parse(testString).tests, singleTest, 'Should output raw test objects with both assertions and test titles');
   t.deepEqual(comments.parse(testStringEmpty).tests, emptyTest, 'Should output a raw test object without an empty assertions array');
 });
