78e477b4d25ec87c53afaeabd6369c2c45a75bfe
Zach Sebag <zsebag@gmail.com>
2015-09-11 19:03:57 -0500

    Added spec to test type of for parsing function

--- tests/parseComments-specs.js
+++ tests/parseComments-specs.js
@@ -5,12 +5,12 @@ var fs = require('fs');
 
 
 test('parse function test', function(t) {
-  t.plan(2);
+  t.plan(4);
   var singleTest = [ { assertions: [ 'sum(6, 7) == 13 (return the sum of both params)', 'sum(8, 9) == 17 (return the sum of both params)' ], loc: { endLine: 5, startLine: 3 }, title: 'sum function' } ];
   var emptyTest =  [ { assertions: [], loc: { endLine: 3, startLine: 3 }, title: 'sum function' } ];
   var testString = fs.readFileSync(path.join(__dirname, 'testDemoSingle.js'), {encoding: 'utf8'});
   var testStringEmpty = fs.readFileSync(path.join(__dirname, 'testDemoEmpty.js'), {encoding: 'utf8'});
-
-  t.deepEqual(comments.parse(testString).tests, singleTest, 'Returns an array of raw test objects with both assertions and titles');
-  t.deepEqual(comments.parse(testStringEmpty).tests, emptyTest, 'Returns an array of empty assertions and test title');
+  t.equal(typeof comments.parse, 'function', 'Should be a function');
+  t.deepEqual(comments.parse(testString).tests, singleTest, 'Should output raw test objects with both assertions and test titles');
+  t.deepEqual(comments.parse(testStringEmpty).tests, emptyTest, 'Should output a raw test object without an empty assertions array');
 });
